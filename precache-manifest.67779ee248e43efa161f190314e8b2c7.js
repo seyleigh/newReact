f39ffb3f9669a9d38ab9734277157e4f0320a38e
Jensey <50929863+seyleigh@users.noreply.github.com>
2020-01-06 18:13:03 -0500

    Updates

---
+++ precache-manifest.67779ee248e43efa161f190314e8b2c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8ce4eb0deb352e78ee92eaf9cc7de73",
+    "url": "/newReact/index.html"
+  },
+  {
+    "revision": "e7b84dbfccb88cba3346",
+    "url": "/newReact/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "283690a59457cb6dab2e",
+    "url": "/newReact/static/js/2.a2cd6f7c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/newReact/static/js/2.a2cd6f7c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7b84dbfccb88cba3346",
+    "url": "/newReact/static/js/main.ff813686.chunk.js"
+  },
+  {
+    "revision": "ce02a92069ccc0df07d0",
+    "url": "/newReact/static/js/runtime-main.d7351fb9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/newReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
